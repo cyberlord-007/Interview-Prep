12d27d38a806fb5d8b341f559d44e1a4fd6c6f96
AKASH CHAUDHARY <2018uec1015@mnit.ac.in>
2020-08-13 23:16:15 +0530

    13/08/20

---
+++ Day#1/ins_sort.cpp
@@ -0,0 +1,37 @@
+// insertion sort
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define w(t)  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define dec(x, y) fixed << setprecision(y) << x
+#define M 1000000007
+
+int main()
+{
+    int i, j, n, x;
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (i = 1; i <= n - 1; i++)
+    {
+        x = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > x)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = x;
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
