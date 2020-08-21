a4ab1f3d802bcd1881b3685699ed56d837c025bc
Akash Chaudhary <akashfzd2000@gmail.com>
2020-08-21 21:50:15 +0530

    deletion at the beginning

--- Linked List/del_begin.cpp
+++ Linked List/del_begin.cpp
@@ -11,7 +11,7 @@ typedef long long int ll;
 struct Node
 {
     int key;
-    Node *next; 
+    Node *next;
     Node(int x)
     {
         key = x;
@@ -27,23 +27,13 @@ void print(Node *head)
     print(head->next);
 }
 
-void insAtEnd(Node *head, int inp)
-{
-    Node *val = new Node(inp);
-    Node *curr = head;
-    while (curr->next != NULL)
-    {
-        curr = curr->next;
-    }
-    curr->next = val;
-}
-
 int main()
 {
-    Node *head = new Node(1);
-    head->next = new Node(2);
-    head->next->next = new Node(3);
-    insAtEnd(head, 4);
-    print(head);
-    return 0;
+    Node *head = new Node(0);
+    head->next = new Node(1);
+    head->next->next = new Node(2);
+    head->next->next->next = new Node(3);
+    Node *temp = head->next;
+    delete head;
+    print(temp);
 }
